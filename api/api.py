8553bffc943e11cce76e676d25041d168c5bb43d
Джеффри <jeffjeffreytagne@gmail.com>
2024-06-01 11:43:08 +0300

    API updated

--- api/api.py
+++ api/api.py
@@ -1,15 +1,36 @@
 import requests
 from config_data.config import RAPID_API_KEY
 
-url = "https://imdb-top-100-movies.p.rapidapi.com/"
 
-headers = {
-    "X-RapidAPI-Key": RAPID_API_KEY,
-    "X-RapidAPI-Host": "imdb-top-100-movies.p.rapidapi.com"
-}
+def get_movies_list(api_key):
+    url = "https://imdb-top-100-movies.p.rapidapi.com/"
 
-response = requests.get(url, headers=headers)
+    headers = {
+        "X-RapidAPI-Key": api_key,
+        "X-RapidAPI-Host": "imdb-top-100-movies.p.rapidapi.com"
+    }
 
-movies_list = response.json()
-# print(len(movies_list))
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return response.status_code
 
+
+def get_series_list(api_key):
+    url = "https://imdb-top-100-movies.p.rapidapi.com/series/"
+
+    headers = {
+        "X-RapidAPI-Key": api_key,
+        "X-RapidAPI-Host": "imdb-top-100-movies.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return response.status_code
+
+
+movies_list = get_movies_list(RAPID_API_KEY)
+series_list = get_series_list(RAPID_API_KEY)
